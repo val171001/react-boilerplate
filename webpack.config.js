8243ca026dbf79a96f40c5cbd663039a291df324
Josue Valenzuela <josue@kemik.gt>
2019-10-08 09:33:27 -0600

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: './src/index.html',
+  filename: 'index.html',
+  inject: 'body'
+})
+
+module.exports = {
+  entry: './src/index.jsx',
+  output: {
+    path: path.resolve('dist'),
+    filename: 'index_bundle.js'
+  },
+  module: {
+    rules: [
+      { test: /\.css$/,
+        use: [
+          { loader: "style-loader" },
+          { loader: "css-loader" }
+        ]
+      },
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: "babel-loader"
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  plugins: [HtmlWebpackPluginConfig]
+}
